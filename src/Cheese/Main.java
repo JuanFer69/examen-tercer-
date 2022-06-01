15f7e3d0ff3361efc104b516c78414b4c0a75e73
Juan <informatica@escuelaces.com>
2022-06-01 11:06:51 +0200

    COMIIT INICIAL

---
+++ src/Cheese/Main.java
@@ -0,0 +1,19 @@
+/*
+*	grupo  :Gdam
+*	alumno : Juan Fernando Falla Rios
+*	n.exp. :7235
+*	fecha  : 01/06/2022
+ */
+package Cheese;
+
+/**
+ *
+ * @author ___
+ */
+public class Main {
+    
+     public static void main(String[] args) {
+    
+         System.out.println("1º Gdam: Entornos de desarrollo");
+    }
+}
